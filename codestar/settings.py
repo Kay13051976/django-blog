a3a77b9d60183782c7366a7927e2cac62e72c199
Kay13051976 <kaywanwisa4@googlemail.com>
2023-11-13 20:03:38 +0000

    Building the admin site by create class PostAdmin and CommentAdmin

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-blog-by-wanwisa.herokuapp.com', 'localhost']
+# ALLOWED_HOSTS = ['django-blog-by-wanwisa.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-kay13051976-django-blog-58zyd33ohg.us2.codeanyapp.com']
 
 
 # Application definition
