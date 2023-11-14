32acc64ecebf173dc879117c8ecbeff206497aa9
Kay13051976 <kaywanwisa4@googlemail.com>
2023-11-14 22:11:37 +0000

    Creat form.py file and add class CommentForm

--- codestar/settings.py
+++ codestar/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
-    # 'crispy_forms',
+    'crispy_forms',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -59,6 +59,8 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # MESSAGE_TAGS = {
 #         messages.DEBUG: 'alert-info',
